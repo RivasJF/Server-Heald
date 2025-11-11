2a9b1556a61e11ee0d269278c23e961745188073
pcArch <jonathariflo@gmail.com>
2025-11-10 22:04:33 -0600

    refactor: improved funcionality of the prisma module

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,28 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy, Logger } from '@nestjs/common';
 import { PrismaClient } from 'generated/prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  private readonly logger = new Logger("Prisma")
+
   async onModuleInit() {
-    await this.$connect();
+    try {
+      await this.$connect();
+      this.logger.log("Database Connect")
+    } catch (error) {
+      this.logger.error("Not connection with Database", error)
+      throw Error
+    }
+    
+  }
+  
+  async onModuleDestroy(){
+    try {
+      await this.$disconnect();
+      this.logger.log("Database Disconnect")
+    } catch (error) {
+      this.logger.error("Can not disconnection with Database")
+    }
+    
   }
 }
