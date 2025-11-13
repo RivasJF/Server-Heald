633f6c2d631287a075108eada12fb6295a629060
pcArch <jonathariflo@gmail.com>
2025-11-12 18:11:14 -0600

    feature: create module user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule, ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true})],
+  imports: [PrismaModule, ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true}), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
