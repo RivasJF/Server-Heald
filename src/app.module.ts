76885e321ea1e9d91fd9a7a471cd38981cc9d3fe
UsPortatil <jonathariflo@gmail.com>
2025-11-18 16:49:58 -0600

    refactor: cleaning app module to controller,service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [PrismaModule, ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true}), UserModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
