574e073b888c0f593a7fddb835d65ec923b60024
UsPortatil <jonathariflo@gmail.com>
2025-11-24 12:38:31 -0600

    feature: create moule doctor

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { DoctorModule } from './doctor/doctor.module';
 
 @Module({
-  imports: [PrismaModule, ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true}), UserModule],
+  imports: [PrismaModule, ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true}), UserModule, DoctorModule],
 })
 export class AppModule {}
