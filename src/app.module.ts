b4c074162a01344cfab2f530b171b7b9f54fe64e
UsPortatil <jonathariflo@gmail.com>
2025-12-05 15:35:37 -0600

    feature: create model and module apoointment

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,9 @@ import { DoctorModule } from './doctor/doctor.module';
 import { AuthModule } from './auth/auth.module';
 import { ScheduleModule } from './schedule/schedule.module';
 import { ClinicModule } from './clinic/clinic.module';
+import { AppointmentModule } from './appointment/appointment.module';
 
 @Module({
-  imports: [PrismaModule, ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true}), UserModule, DoctorModule, AuthModule, ScheduleModule, ClinicModule],
+  imports: [PrismaModule, ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true}), UserModule, DoctorModule, AuthModule, ScheduleModule, ClinicModule, AppointmentModule],
 })
 export class AppModule {}
