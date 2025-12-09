ec90d484a72fdb365c249303f5afbc3f67caadb8
UsPortatil <jonathariflo@gmail.com>
2025-12-08 20:51:53 -0600

    refactor: tranform email login

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,9 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class LoginDto {
   @ApiProperty({ example: 'james.madison@examplepetstore.com' })
   @IsEmail()
+  @Transform(({ value }) => value?.toLowerCase())
   email: string;
 
   @ApiProperty({ example: '12345678' })
